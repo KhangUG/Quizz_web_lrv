5b098a4c9c18a91c619c7869d80a59c9b7390157
Khangug <khang123@gmail.com>
2024-06-28 02:06:08 +0700

    Tao lien ket dong cho cac bai kiem tra

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -28,6 +28,13 @@
             <td>{{ $exam->date}}</td>
             <td>{{ $exam->time }}Hrs</td>
             <td>{{ $exam->attempt}} Time</td>
+            <td></td>
+            <td>
+                <a href="#" data-code="{{ $exam-> enterance_id}}" class="copy">
+                    <i class="fa fa-copy"></i>
+                </a>
+            </td>
+
         </tr>
         @endforeach
         @else
@@ -37,5 +44,38 @@
         @endif
     </tbody>
 </table>
+
+<script>
+$(document).ready(function() {
+
+    //tao ra doan ma de student click vao roi lay link ra bai kiem tra
+    $('.copy').click(function() {
+        // Thêm phần tử span với lớp "copied_text" và nội dung "Copied" vào trước phần tử cha của phần tử bị nhấn
+        $(this).parent().prepend('<span class="copied_text">Copied</span>');
+
+        // Lấy giá trị của thuộc tính data-code từ phần tử bị nhấn và lưu vào biến code
+        var code = $(this).attr('data-code');
+        // Tạo URL của bài thi bằng cách nối URL gốc với "/exam/" và mã của bài thi
+        var url = "{{ URL::to('/') }}/exam/" + code;
+
+        // Tạo một phần tử input tạm thời
+        var $temp = $("<input>");
+        // Thêm phần tử này vào body của tài liệu
+        $("body").append($temp);
+        // Đặt giá trị của phần tử input là URL và chọn giá trị đó
+        $temp.val(url).select();
+        // Sao chép giá trị được chọn vào clipboard
+        document.execCommand("copy");
+        // Loại bỏ phần tử input tạm thời
+        $temp.remove();
+
+        // Sử dụng setTimeout để đợi 1 giây (1000ms)
+        setTimeout(() => {
+            // Sau 1 giây, xóa phần tử span có lớp "copied_text" đã thêm ở trên
+            $('.copied_text').remove();
+        }, 1000);
+    });
+});
+</script>
 
 @endsection
