eb0aa77544d06253996893c6750e142682b551cc
Khangug <khang123@gmail.com>
2024-07-01 09:47:33 +0700

    them diem pass cho sv

--- resources/views/admin/marksDashboard.blade.php
+++ resources/views/admin/marksDashboard.blade.php
@@ -11,6 +11,7 @@
         <th>Exam Name</th>
         <th>Marks/Q</th>
         <th>Total Marks</th>
+        <th>Passing Marks</th>
         <th>Edit</th>
     </thead>
     <tbody>
@@ -23,8 +24,9 @@
                     <td>{{ $exam->exam_name }}</td>
                     <td>{{ $exam->marks }}</td>
                     <td>{{ count($exam->getQnaExam) * $exam->marks }}</td>
+                    <td>{{ $exam->pass_marks }}</td>
                     <td>
-                        <button class="btn btn-primary editMarks" data-id="{{ $exam->id }}" data-marks="{{ $exam->marks }}" data-totalq="{{ count($exam->getQnaExam) }}" data-toggle="modal"
+                        <button class="btn btn-primary editMarks" data-id="{{ $exam->id }}" data-pass-marks="{{ $exam->pass_marks }}" data-marks="{{ $exam->marks }}" data-totalq="{{ count($exam->getQnaExam) }}" data-toggle="modal"
                     data-target="#editMarksModel">Edit</button>
                     </td>
                 </tr>
@@ -55,7 +57,7 @@
                 @csrf
                 <div class="modal-body">
                     <div class="row">
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <label >Marks/Q</label>
                         </div>
 
@@ -67,14 +69,26 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-sm-3">
+                    <div class="row mt-2">
+                        <div class="col-sm-4">
                             <label >Total Marks</label>
                         </div>
 
                         <div class="col-sm-6">
                            
                             <input type="text" disabled  placeholder="Total Marks" id="tmarks"  >
+                        </div>
+                    </div>
+
+                    <div class="row mt-2">
+                        <div class="col-sm-4">
+                            <label >Passing Marks</label>
+                        </div>
+
+                        <div class="col-sm-6">
+                           <input type="text" 
+                                onkeypress="return event.charCode >= 48 && event.charCode <=57 || event.charCode == 46"
+                            name="pass_marks" placeholder="Enter Passng Marks" id="pass_marks" required>
                         </div>
                     </div>
 
@@ -103,14 +117,54 @@ $(document).ready(function() {
         $('#tmarks').val((marks * totalq).toFixed(1));
 
         totalQna = totalq;
+        $('#pass_marks').val($(this).attr('data-pass-marks'));
+
     });
 
     $('#marks').keyup(function() {
         $('#tmarks').val(($(this).val() * totalQna).toFixed(1));
     });
 
+    // Bắt sự kiện khi người dùng nhấn phím và thả phím trong input có id 'pass_marks'
+    $('#pass_marks').keyup(function() {
+        $('.pass-error').remove();
+
+        // Lấy giá trị từ input có id 'tmarks'
+        var tmarks = $('#tmarks').val();
+        // Lấy giá trị từ input hiện tại (id 'pass_marks')
+        var pmarks = $(this).val();
+
+        // Kiểm tra nếu giá trị trong 'pass_marks' lớn hơn hoặc bằng 'tmarks'
+        if (parseFloat(pmarks) >= parseFloat(tmarks)) {
+            // Thêm một đoạn văn bản màu đỏ có nội dung 'X' vào phần tử cha của input hiện tại
+            $(this).parent().append('<p style="color:red;" class="pass-error">Passing marks will be less than total mark</p>');
+            setTimeout(() => {
+                $('.pass-error').remove();
+            }, 2000);
+        }
+    });
+
+
     $('#editMarks').submit(function(event) {
         event.preventDefault();
+
+        $('.pass-error').remove();
+
+        // Lấy giá trị từ input có id 'tmarks'
+        var tmarks = $('#tmarks').val();
+        // Lấy giá trị từ input hiện tại (id 'pass_marks')
+        var pmarks = $('#pass_marks').val();
+
+        // Kiểm tra nếu giá trị trong 'pass_marks' lớn hơn hoặc bằng 'tmarks'
+        if (parseFloat(pmarks) >= parseFloat(tmarks)) {
+            // Thêm một đoạn văn bản màu đỏ có nội dung 'X' vào phần tử cha của input hiện tại
+            $('#pass_marks').parent().append('<p style="color:red;" class="pass-error">Passing marks will be less than total mark</p>');
+            setTimeout(() => {
+                $('.pass-error').remove();
+            }, 2000);
+
+            return false;
+        }
 
         var formData = $(this).serialize();
 
